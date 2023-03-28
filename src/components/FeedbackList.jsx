879aeedc354107a1c1ea1769f1eaf97c04758cc7
Davy De Groote <davy.degr@gmail.com>
2023-03-28 16:45:04 +0200

    Added animation on adding/deleting reviews

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,5 +1,6 @@
 import FeedbackItem from './FeedbackItem'
 import PropTypes from 'prop-types'
+import {motion, AnimatePresence} from 'framer-motion'
 
 function FeedbackList({ feedback, handleDelete }) {
 
@@ -9,14 +10,36 @@ function FeedbackList({ feedback, handleDelete }) {
 
   return (
     <div className ='feedback-list'>
+      <AnimatePresence>
       {feedback.map((item) => {
-        return <FeedbackItem
-                  key={item.id}
-                  item={item}
-                  handleDelete={handleDelete}/>
+        return (
+          <motion.div
+            key={item.id}
+            initial={{opacity: 0}}
+            animate={{opacity: 1}}
+            exit={{opacity: 0}}
+          >
+            <FeedbackItem
+                      key={item.id}
+                      item={item}
+                      handleDelete={handleDelete}/>
+          </motion.div>
+          )
       })}
+      </AnimatePresence>
     </div>
   )
+
+  // return (
+  //   <div className ='feedback-list'>
+  //     {feedback.map((item) => {
+  //       return <FeedbackItem
+  //                 key={item.id}
+  //                 item={item}
+  //                 handleDelete={handleDelete}/>
+  //     })}
+  //   </div>
+  // )
 }
 
 FeedbackList.propTypes = {
