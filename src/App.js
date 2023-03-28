b008ba3e555edf2596fc3b48a9bcfa77735a54dd
Davy De Groote <davy.degr@gmail.com>
2023-03-28 15:34:42 +0200

    Added submit feedback form

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from "./components/Header"
 import FeedbackData from "./data/FeedbackData"
 import FeedbackList from "./components/FeedbackList"
 import FeedbackStats from "./components/FeedbackStats"
+import FeedbackForm from "./components/FeedbackForm"
 
 function App() {
 
@@ -22,9 +23,12 @@ function App() {
     <>
       <Header />
 
-      <FeedbackStats feedback={feedback}/>
 
       <div className="container">
+        <FeedbackForm />
+
+        <FeedbackStats feedback={feedback}/>
+
         <FeedbackList
           feedback={feedback}
           handleDelete={deleteFeedback} />
---
+++ src/components/FeedbackForm.jsx
@@ -0,0 +1,32 @@
+import Card from "./shared/Card"
+import {useState} from 'react'
+
+function FeedbackForm() {
+
+  const [text, setText] = useState('')
+
+  const handleTextChange = (event) => {
+    setText(event.target.value)
+  }
+
+  return (
+    <Card>
+      <form action="#">
+        <h2>How would you rate your service with us?</h2>
+
+        {/* TODO: Rating selecto component */}
+
+        <div className="input-group">
+          <input onChange={handleTextChange}
+                 type="text"
+                 placeholder="Write a review"
+                 value={text} />
+          <button type="submit">Send</button>
+        </div>
+      </form>
+    </Card>
+  )
+
+}
+
+export default FeedbackForm
